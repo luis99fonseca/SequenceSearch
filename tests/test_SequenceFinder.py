a6fa1b799595df9e1e367920b38f6c22559849be
Luis Fonseca <luiscdf@ua.pt>
2022-12-16 16:42:12 +0000

    small refactor to testing file

--- tests/test_SequenceFinder.py
+++ tests/test_SequenceFinder.py
@@ -8,29 +8,33 @@ class TestSequenceFinder(unittest.TestCase):
         print("Creating new SequenceFinder...")
         self.sequenceFinder = SequenceFinder.SequenceFinder()
 
+    def executeSequenceFinder_basicPipeline(self, file_name):
+        self.sequenceFinder.read_input(file_name)
+        return self.sequenceFinder.compute_sequence()
+
     def test_case01(self):
-        self.sequenceFinder.read_input("test01.txt")
-        result = self.sequenceFinder.compute_sequence()
+        print("Executing Test01...")
+        result = self.executeSequenceFinder_basicPipeline("test01.txt")
         self.assertEqual(result, [1, 2, 3, 4])
 
     def test_case02(self):
-        self.sequenceFinder.read_input("test02.txt")
-        result = self.sequenceFinder.compute_sequence()
+        print("Executing Test02...")
+        result = self.executeSequenceFinder_basicPipeline("test02.txt")
         self.assertEqual(result, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
 
     def test_case03(self):
-        self.sequenceFinder.read_input("test03.txt")
-        result = self.sequenceFinder.compute_sequence()
+        print("Executing Test03...")
+        result = self.executeSequenceFinder_basicPipeline("test03.txt")
         self.assertEqual(result, [1, 2, 3, 4])
 
     def test_case04(self):
-        self.sequenceFinder.read_input("test04.txt")
-        result = self.sequenceFinder.compute_sequence()
+        print("Executing Test04...")
+        result = self.executeSequenceFinder_basicPipeline("test04.txt")
         self.assertEqual(result, [5, 6, 7, 8, 9])
 
     def test_case05(self):
-        self.sequenceFinder.read_input("test05.txt")
-        result = self.sequenceFinder.compute_sequence()
+        print("Executing Test05...")
+        result = self.executeSequenceFinder_basicPipeline("test05.txt")
         self.assertEqual(result, [5, 6, 7, 8, 9])
 
 if __name__ == '__main__':
